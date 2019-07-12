b111e13fb113d35e11b595a91f519bf2047018c8
guoyu <120098866@qq.com>
2019-07-12 14:08:40 +0800

    提交自动部署脚本

--- scp.sh
+++ scp.sh
@@ -4,14 +4,15 @@ set timeout 3000
 set File [lindex $argv 0]
 set Target [lindex $argv 1]
 set Host [lindex $argv 2]
-set Pass [lindex $argv 3]
+set Port [lindex $argv 3]
+set Pass [lindex $argv 4]
 
 if { ! ([file exists /usr/bin/scp] && [file executable /usr/bin/scp])} {
     puts stderr "/usr/bin/scp does not exist or is not executable"
     exit 7
 }
 
-spawn /usr/bin/scp -r -P $File root@$Host:$Target
+spawn /usr/bin/scp -r -P $Port $File root@$Host
 expect {
 "yes/no" { send "yes\r"; exp_continue}
 "password:" { send "$Pass\r" }
