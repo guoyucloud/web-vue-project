3dc29efd30f6b4b944565770ef08ba6d4784419e
guoyu <120098866@qq.com>
2019-07-22 14:18:48 +0800

    eslint配置完毕

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,41 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
+  extends: [
+    'plugin:vue/strongly-recommended',
     '@vue/standard'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-useless-escape': 'off',
+    'indent': ['error', 'tab'],
+    'no-tabs': 'off',
+    'vue/html-indent': ['error', 'tab'],
+    'vue/component-name-in-template-casing': 'off',
+    'vue/max-attributes-per-line': ['warn', {
+      'singleline': 3,
+      'multiline': {
+        'max': 1,
+        'allowFirstLine': false
+      }
+    }]
   },
   parserOptions: {
     parser: 'babel-eslint'
-  }
+  },
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        indent: 'off'
+      }
+    }
+  ],
+  plugins: [
+    'vue'
+  ]
 }
+
